0f6ea76e2f53c4e1a171f02f7b99d9aa9db4dc28
Camila-Ribeiro <camilarf@gmail.com>
2020-08-17 16:52:13 +0000

    Added products views and templates

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -30,4 +30,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-        
