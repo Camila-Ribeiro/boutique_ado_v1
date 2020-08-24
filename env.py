c75733d9ced758b370ec35c4b8aa7751eba20be1
Camila-Ribeiro <camilarf@gmail.com>
2020-08-24 20:04:48 +0000

    Added checkout sucess logic

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51HJIlwHugkRJs5ye6qRLA3xrypq4lI3Wdmqt4gnkSC5TBn29tTAkQWJWBzoIuEMDSnEGwKBgUQ6FjBw3amuGeJ1100DGf0z8wB")
+os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51HJIlwHugkRJs5yejTAsatgDZR7Tkefgn9yRFGicj5yf5XG5iPshzjbR2NdMUpAPcsiBxF8H1bIXxegwXqRKyYuX008hFGvXMC")
