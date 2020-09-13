681d2991cf57e423108859a5fb456456c7caabc7
Camila <camilarf@gmail.com>
2020-09-13 14:06:44 +0100

    Added database-url to env.py

--- env.py
+++ env.py
@@ -4,3 +4,4 @@ os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51HJIlwHugkRJs5ye6qRLA3xrypq
 os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51HJIlwHugkRJs5yejTAsatgDZR7Tkefgn9yRFGicj5yf5XG5iPshzjbR2NdMUpAPcsiBxF8H1bIXxegwXqRKyYuX008hFGvXMC")
 os.environ.setdefault("STRIPE_WH_SECRET", "whsec_DEFFkcuyhLxQAwleEODff6sQwI1Cj80Y")
 os.environ.setdefault("SECRET_KEY", "06t_gc&pg(5e%^6(us3v7d!ks0#hqydkja_-6n(b9d)uy96_sd")
+os.environ.setdefault("DATABASE_URL","postgres://kmmluvwinsleac:5d0bc1c6868f0e0210df8d02e5774bb944b57f62dc43eb7b61c9c08bcc75006a@ec2-52-31-94-195.eu-west-1.compute.amazonaws.com:5432/d9dla77j17jjta")
